7d340a7b8ea1a3583f518cb34a61c1b95b01fd6f
Dan Manning <dmanning@oceaneering.com>
2021-11-03 17:50:20 -0500

    Added bridge project

--- UndoRedoBuddy/buildNugetPackages.ps1
+++ UndoRedoBuddy/buildNugetPackages.ps1
@@ -1,4 +1,5 @@
 rm *.nupkg
 nuget pack .\UndoRedoBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\UndoRedoBuddy.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
 nuget push -source https://www.nuget.org -NonInteractive *.nupkg

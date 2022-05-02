7e1b94cde52df128b8393169d092b70e445b1b85
Dan Manning <dmanning@oceaneering.com>
2022-05-02 16:41:42 -0500

    removed bridge proj

--- UndoRedoBuddy/buildNugetPackages.ps1
+++ UndoRedoBuddy/buildNugetPackages.ps1
@@ -1,5 +1,4 @@
 rm *.nupkg
 nuget pack .\UndoRedoBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\UndoRedoBuddy.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
 nuget push -source https://www.nuget.org -NonInteractive *.nupkg
